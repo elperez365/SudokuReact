d3ff77b6ae44b6a00d932431da1a0775fa3f1739
Vasco Ceti <vasco.ceti@gmail.com>
2024-04-09 13:43:51 +0200

    feat(ListActions) add async handler for buttons

--- frontend/src/components/ListActions.tsx
+++ frontend/src/components/ListActions.tsx
@@ -2,16 +2,35 @@ import { useContext } from "react";
 import Button from "./ui/Button";
 import { GameContextType } from "../types/GameContext";
 import { GameContext } from "../context/GameContext";
+import { getValidSudoku, getClearSudokuList } from "@api/axios";
 
 const ListActions: React.FC<{}> = () => {
   const { handleUpdateList } = useContext<GameContextType>(GameContext);
 
+  const handleAddValidSudoku = async () => {
+    try {
+      await getValidSudoku();
+      handleUpdateList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClearList = async () => {
+    try {
+      await getClearSudokuList();
+      handleUpdateList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex gap-2">
-      <Button color="tertiary" onClick={() => console.log("add Correct")}>
+      <Button color="tertiary" onClick={() => handleAddValidSudoku()}>
         Add Valid Sudoku
       </Button>
-      <Button color="secondary" onClick={() => console.log("clear")}>
+      <Button color="secondary" onClick={() => handleClearList()}>
         Clear List
       </Button>
     </div>
